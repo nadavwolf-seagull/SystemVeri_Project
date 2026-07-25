f587900e7b4b31ba09ba7294fe87b3c1e0ea14fe
Shahar Pines <shaharh50@gmail.com>
2026-07-25 16:13:28 +0300

    Reorganize DMA UVM environment structure

--- uvm_tb/sim/verilated_random.d
+++
@@ -1,8 +0,0 @@
-verilated_random.o: \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_random.cpp \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_random.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_config.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilatedos.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_types.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_funcs.h
--- uvm_tb/sim/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_timing.cpp \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_timing.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_config.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilatedos.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_types.h \
- /home/shaharpi/my_projects/cdv_feb26/third-party/verilator/include/verilated_funcs.h
